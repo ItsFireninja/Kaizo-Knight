1a8e2fc55b46952d42c8c2cc566afefc99187f16
Noah Lin <91566343+ItsFireninja@users.noreply.github.com>
2021-10-05 12:21:28 -0500

    Create assets.js

---
+++ assets.js
@@ -0,0 +1,18 @@
+export default function loadAssets() {
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("googoly", "sprites/googoly.png");
+  loadSprite("fliboi", "sprites/fliboi.png");
+	loadSprite("spike", "sprites/spike.png");
+  loadSprite("dangergrass", "sprites/ded-grass.png");
+	loadSprite("grass", "sprites/grass.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png");
+  loadSprite("bg", "sprites/background.png");
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("coin", "sprites/coin.png");
+	loadSound("coin", "sounds/score.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/hit.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+}
